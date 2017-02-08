a74cb185d43ebe74768d689f03be6e532bd67e96
ProfDema <ilir.dema@mail.utoronto.ca>
2017-02-08 12:45:02 -0500

    Starter Code

---
+++ A4/src/main/java/org/csc301/GameTest.java
@@ -0,0 +1,24 @@
+package org.csc301;
+
+
+import java.io.FileNotFoundException;
+
+// This class is not part of your evaluation. You may use it for testing if you want.
+public class GameTest {
+
+	/**
+	 * @param args
+	 * @throws HeapEmptyException 
+	 * @throws HeapFullException 
+	 * @throws FileNotFoundException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException, HeapFullException, HeapEmptyException {
+		// TODO Auto-generated method stub
+		TreasureHunt game = new TreasureHunt();
+        game.play("game.txt");
+        System.out.println(game.getMap());
+        System.out.println(game.state);
+        System.out.println(game.pathLength());
+	}
+
+}
---
+++ A4/src/main/java/org/csc301/HeapItem.java
@@ -0,0 +1,8 @@
+package org.csc301;
+
+public interface HeapItem extends Comparable<HeapItem>{
+	
+	public void setHeapIndex(int index);
+	public int getHeapIndex();
+
+}
