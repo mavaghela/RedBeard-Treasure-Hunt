cc1cc11db23c061291de2421fe7e3c148e237ee9
Ryan Llewellin <ryanllewellin@icloud.com>
2017-02-24 17:42:10 -0500

    not compiling

---
+++ A4/src/main/java/org/csc301/TreasureHuntTest.java
@@ -0,0 +1,11 @@
+package org.csc301;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+import org.csc301.Heap;
+
+public class TreasureHuntTest {
+    
+}
